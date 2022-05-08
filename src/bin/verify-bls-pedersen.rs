034350c851bb7799ccb58c289c837f148eebee37
Aleixo Sanchez <15819210+alxs@users.noreply.github.com>
2022-05-08 15:27:51 +0200

    solve using ndarray

--- src/bin/verify-bls-pedersen.rs
+++ src/bin/verify-bls-pedersen.rs
@@ -12,6 +12,15 @@ use prompt::{puzzle, welcome};
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
 
+#[macro_use]
+extern crate ndarray;
+extern crate ndarray_linalg;
+
+use ndarray::arr2;
+use ndarray::prelude::*;
+use ndarray::{Array, Array1};
+use ndarray_linalg::Solve;
+
 fn main() {
     welcome();
     puzzle(PUZZLE_DESCRIPTION);
@@ -25,7 +34,7 @@ fn main() {
         1, 1,
     ]);
     let message = "a&m".as_bytes();
-    let (_, m) = hash_to_curve(message);
+    let (m_bytes, m_proj) = hash_to_curve(message);
 
     let ms_iter: Vec<f32> = ms
         .iter()
@@ -45,8 +54,25 @@ fn main() {
         .collect();
 
     println!("{:?}", ms_iter);
-    let M = SMatrix::<f32, 256, 256>::from_vec(ms_iter);
+    // let M = SMatrix::<f32, 256, 256>::from_vec(ms_iter);
 
+    let m_bits: Vec<f32> = m_bytes
+        .iter()
+        .flat_map::<_, _>(|c| {
+            let bits: Vec<f32> = BitVec::<_, Msb0>::from_element(*c)
+                .iter()
+                .map(|b| f32::from(u8::from(*b)))
+                .collect();
+            bits
+        })
+        .collect();
+
+    let a = Array::from_shape_vec((256, 256), ms_iter).unwrap();
+    let b: Array1<f32> = m_bits.into();
+
+    let x = a.solve_into(b).unwrap();
+
+    println!("{:?}", x);
     // let inv = M.rank();
     // println!("{:?}", inv);
     // try_inverse().unwrap();
