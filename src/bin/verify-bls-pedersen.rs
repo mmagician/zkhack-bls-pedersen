ae07951f8a7a8f59f7ffeab563f7a08ebdc794fd
Aleixo Sánchez <15819210+alxs@users.noreply.github.com>
2022-05-10 09:00:18 +0200

    WIP

--- src/bin/verify-bls-pedersen.rs
+++ src/bin/verify-bls-pedersen.rs
@@ -1,20 +1,153 @@
-use bls_pedersen::bls::verify;
+use ark_bls12_381::{G1Affine, G1Projective, G2Affine};
+use ark_ec::AffineCurve;
+use ark_ec::ProjectiveCurve;
+use ark_serialize::CanonicalDeserialize;
+use bitvec::prelude::*;
 use bls_pedersen::data::puzzle_data;
-use bls_pedersen::PUZZLE_DESCRIPTION;
-use prompt::{puzzle, welcome};
+use bls_pedersen::verify;
+use nalgebra::*;
+use ndarray::arr2;
+use num_bigint::BigInt;
+use num_traits::identities::Zero;
+use std::convert::TryInto;
+use std::error::Error;
+use std::io::Cursor;
+use std::ops::Mul;
+use std::ops::Neg;
+type F = fraction::Fraction;
+use ark_ff::Field;
+use ark_ff::{field_new, fields::*};
+use ark_serialize::CanonicalSerialize;
+use ark_bls12_381::Fq;
 
-fn main() {
-    welcome();
-    puzzle(PUZZLE_DESCRIPTION);
+fn explode_u8(b: &u8) -> [u8; 8] {
+    [
+        (b >> 7) & 0x1,
+        (b >> 6) & 0x1,
+        (b >> 5) & 0x1,
+        (b >> 4) & 0x1,
+        (b >> 3) & 0x1,
+        (b >> 2) & 0x1,
+        (b >> 1) & 0x1,
+        b & 0x1,
+    ]
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let (pk, ms, sigs) = puzzle_data();
-    for (m, sig) in ms.iter().zip(sigs.iter()) {
-        verify(pk, m, *sig);
+    println!("Verifying existing messages...");
+    let mut i = 0;
+    // for (m, sig) in ms.iter().zip(sigs.iter()) {
+    //     verify(pk, m, *sig);
+    //     println!("{}/{}", i, ms.len());
+    //     i = i + 1;
+    // }
+    println!("Done!");
+
+    let m = "alxs".as_bytes();
+    let hash = blake2s_simd::blake2s(m)
+        .as_bytes()
+        .to_vec()
+        .into_iter()
+        .flat_map(|b| explode_u8(&b))
+        .map(|u| u as f32)
+        .collect::<Vec<_>>();
+
+    // println!("{:?}", m_b2hash);
+
+    let ms_hashes = ms
+        .iter()
+        .flat_map(|m| {
+            blake2s_simd::blake2s(m)
+                .as_bytes()
+                .to_vec()
+                .into_iter()
+                .flat_map(|b| explode_u8(&b))
+                .map(|u| u as f32)
+        })
+        .collect::<Vec<_>>();
+
+    // println!("{:?}", ms_hashes);
+
+    // Solve A x = v
+    let v = DMatrix::from_vec(256, 1, hash);
+    let mx = DMatrix::from_vec(256, 256, ms_hashes);
+    println!("About to");
+    // println!("{}", mx.is_invertible());
+
+    let hell_yeah = mx
+        .lu()
+        .solve(&v)
+        .unwrap()
+        .data
+        .as_vec()
+        .iter()
+        .map(|h: &f32| F::from(*h * 50000000 as f32))
+        .collect::<Vec<_>>();
+
+    // let v: f32 = 1;
+    // let d = v * 1000000;
+    // let mx_inv = match mx.try_inverse() {
+    //     Some(m) => m,
+    //     None => return Ok(()),
+    // };
+    println!("{:?}", hell_yeah);
+
+    // let hell_yeah = &v * &mx_inv;
+    // println!("{:?}", hell_yeah);
+
+    // let sigs_mx = DMatrix::from_row_iter(
+    //     256,
+    //     48,
+    //     sigs.iter().map(|s| s.to_vec()).flatten().map(|u| u as i8),
+    // );
+
+    // let sig_m = hell_yeah * sigs_mx;
+
+    let mut i = 0;
+    let mut acc = G1Projective::zero();
+    for (factor, sig) in hell_yeah.iter().zip(sigs.iter()) {
+        let numer = match factor.numer() {
+            Some(num) => num,
+            None => return Ok(()),
+        };
+        let denom = match factor.denom() {
+            Some(num) => num,
+            None => return Ok(()),
+        };
+
+        //TODO this is wrong
+        let mut res = sig.mul(*numer);
+        // res -= sig.mul(*denom);
+
+        if let fraction::Sign::Minus = factor.sign().unwrap() {
+            res = -res;
+        };
+
+        acc += res;
     }
 
-    /* Your solution here! */
-    /*
-      let sig = ...;
-      let m = your username;
-      verify(pk, m, sig);
-    */
+    let det: ark_bls12_381::Fq2 = field_new!(Fq, "50000000");
+    acc.into_affine().verify(pk, m, acc.into());
+
+    // let sigs = sigs_strs
+    //     .iter()
+    //     .map(|&s| G1Affine::deserialize(&mut Cursor::new(hex::decode(s).unwrap())).unwrap())
+    //     .collect();
+
+    // let sig =
+    // G1Affine::deserialize(&mut Cursor::new(hex::decode(sig_m.as_vector()).unwrap())).unwrap();
+
+    // verify(pk, m, sig);
+
+    // let mut sum: Vec<u8> = [0; 256].to_vec();
+    // for m in ms {
+    //     sum = sum
+    //         .iter()
+    //         .zip(m.iter())
+    //         .map(|(&b, &v)| b + v)
+    //         .collect();
+    // }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,48 @@
-pub mod bls;
 pub mod data;
-pub mod hash;
 
-pub const PUZZLE_DESCRIPTION: &str = r#"Alice designed an authentication system in which users gain access by presenting it a signature on a username, which Alice provided.
-One day, Alice discovered 256 of these signatures were leaked publicly, but the secret key wasn't. Phew.
-The next day, she found out someone accessed her system with a username she doesn't know! This shouldn't be possible due to existential unforgeability, as she never signed such a message.
+use ark_bls12_381::{Bls12_381, G1Affine, G1Projective, G2Affine};
+use ark_crypto_primitives::crh::{
+    pedersen::{Window, CRH},
+    CRH as CRHScheme,
+};
+use ark_ec::{AffineCurve, PairingEngine};
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 
-Can you find out how it happend and produce a signature on your username?"#;
+use std::ops::Neg;
+
+use ark_ff::One;
+
+#[derive(Clone)]
+struct ZkHackPedersenWindow {}
+
+impl Window for ZkHackPedersenWindow {
+    const WINDOW_SIZE: usize = 1;
+    const NUM_WINDOWS: usize = 256;
+}
+
+pub fn verify(pk: G2Affine, msg: &[u8], sig: G1Affine) {
+    let (_, h) = hash_to_curve(msg);
+    assert!(Bls12_381::product_of_pairings(&[
+        (
+            sig.into(),
+            G2Affine::prime_subgroup_generator().neg().into()
+        ),
+        (h.into(), pk.into()),
+    ])
+    .is_one());
+}
+
+pub fn hash_to_curve(msg: &[u8]) -> (Vec<u8>, G1Affine) {
+    let rng_pedersen = &mut ChaCha20Rng::from_seed([
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1,
+    ]);
+    let parameters = CRH::<G1Projective, ZkHackPedersenWindow>::setup(rng_pedersen).unwrap();
+    let b2hash = blake2s_simd::blake2s(msg);
+    (
+        b2hash.as_bytes().to_vec(),
+        CRH::<G1Projective, ZkHackPedersenWindow>::evaluate(&parameters, b2hash.as_bytes())
+            .unwrap(),
+    )
+}
