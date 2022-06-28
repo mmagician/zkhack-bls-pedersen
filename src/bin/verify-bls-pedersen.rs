daf06a130d0a5b5b99aba454e08a98427ba26226
Aleixo Sanchez <15819210+alxs@users.noreply.github.com>
2022-06-28 19:42:38 +0200

    finish idea

--- src/bin/verify-bls-pedersen.rs
+++ src/bin/verify-bls-pedersen.rs
@@ -1,42 +1,22 @@
 use ark_bls12_381::{G1Affine, G1Projective, G2Affine};
+use ark_crypto_primitives::crh::pedersen::bytes_to_bits;
 use ark_ec::AffineCurve;
 use ark_ec::ProjectiveCurve;
-use ark_serialize::CanonicalDeserialize;
-use bitvec::prelude::*;
 use bls_pedersen::data::puzzle_data;
 use bls_pedersen::verify;
 use nalgebra::*;
-use ndarray::arr2;
-use num_bigint::BigInt;
+use num::integer::lcm;
 use num_traits::identities::Zero;
-use std::convert::TryInto;
 use std::error::Error;
-use std::io::Cursor;
-use std::ops::Mul;
-use std::ops::Neg;
 type F = fraction::Fraction;
+use ark_bls12_381::Fr;
+use ark_ff::fields::*;
 use ark_ff::Field;
-use ark_ff::{field_new, fields::*};
-use ark_serialize::CanonicalSerialize;
-use ark_bls12_381::Fq;
-
-fn explode_u8(b: &u8) -> [u8; 8] {
-    [
-        (b >> 7) & 0x1,
-        (b >> 6) & 0x1,
-        (b >> 5) & 0x1,
-        (b >> 4) & 0x1,
-        (b >> 3) & 0x1,
-        (b >> 2) & 0x1,
-        (b >> 1) & 0x1,
-        b & 0x1,
-    ]
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (pk, ms, sigs) = puzzle_data();
     println!("Verifying existing messages...");
-    let mut i = 0;
+    // let mut i = 0;
     // for (m, sig) in ms.iter().zip(sigs.iter()) {
     //     verify(pk, m, *sig);
     //     println!("{}/{}", i, ms.len());
@@ -45,12 +25,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Done!");
 
     let m = "alxs".as_bytes();
-    let hash = blake2s_simd::blake2s(m)
-        .as_bytes()
-        .to_vec()
+    let hash = bytes_to_bits(blake2s_simd::blake2s(m).as_bytes())
         .into_iter()
-        .flat_map(|b| explode_u8(&b))
-        .map(|u| u as f32)
+        .map(|x| x as i32 as f32)
         .collect::<Vec<_>>();
 
     // println!("{:?}", m_b2hash);
@@ -58,12 +35,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ms_hashes = ms
         .iter()
         .flat_map(|m| {
-            blake2s_simd::blake2s(m)
-                .as_bytes()
-                .to_vec()
+            bytes_to_bits(blake2s_simd::blake2s(m).as_bytes())
                 .into_iter()
-                .flat_map(|b| explode_u8(&b))
-                .map(|u| u as f32)
+                .map(|x| x as i32 as f32)
         })
         .collect::<Vec<_>>();
 
@@ -77,9 +51,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     // So if some combination of input hashes = h, then the same combination of S_i = S(h)
     // S(h) = [sk].( \sigma(a_i[1]).G_1 )
     // \sigma(S_i) = ( \sigma(a_i[1]) ).( [sk].G_1 )
-    // x: linear representation of v in basis formed by A - the set of hashes of all input messages
-    // v: our hash i.e. our 
+    // A: the set of hashes of all input messages
+    // x: linear representation of v in basis formed by A
+    // v: our hash i.e. our factors for the G_i
 
+    // Alternatively, maybe:
     // we wish to solve:
     // m_0.[sk[G_0]] + ... + m_n.[sk.[G_n]] = B,
     // where B is the signature
@@ -101,8 +77,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         .data
         .as_vec()
         .iter()
-        .map(|h: &f32| F::from(*h * 50000000 as f32))
+        .map(|h: &f32| F::from(*h))
         .collect::<Vec<_>>();
+
+    let lcm = hell_yeah
+        .iter()
+        .fold(1, |acc, x| lcm(acc, *x.denom().unwrap()));
+    println!("LCM: {}", lcm);
 
     // let v: f32 = 1;
     // let d = v * 1000000;
@@ -123,31 +104,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // let sig_m = hell_yeah * sigs_mx;
 
-    // let mut i = 0;
-    // let mut acc = G1Projective::zero();
-    // for (factor, sig) in hell_yeah.iter().zip(sigs.iter()) {
-    //     let numer = match factor.numer() {
-    //         Some(num) => num,
-    //         None => return Ok(()),
-    //     };
-    //     let denom = match factor.denom() {
-    //         Some(num) => num,
-    //         None => return Ok(()),
-    //     };
+    let mut acc = G1Projective::zero();
+    for (factor, sig) in hell_yeah.iter().zip(sigs.iter()) {
+        let numer = factor.numer().unwrap();
+        let denom = factor.denom().unwrap();
 
-    //     //TODO this is wrong
-    //     let mut res = sig.mul(*numer);
-    //     // res -= sig.mul(*denom);
+        println!("factor: {}", lcm / denom * numer);
+        let res = lcm / denom * numer;
+        // res -= sig.mul(*denom);
 
-    //     if let fraction::Sign::Minus = factor.sign().unwrap() {
-    //         res = -res;
-    //     };
+        if let fraction::Sign::Minus = factor.sign().unwrap() {
+            acc -= sig.mul(res);
+        } else {
+            acc += sig.mul(res);
+        }
+    }
 
-    //     acc += res;
-    // }
-
-    // let det: ark_bls12_381::Fq2 = field_new!(Fq, "50000000");
-    // acc.into_affine().verify(pk, m, acc.into());
+    acc = acc.mul(&Fr::from(lcm).inverse().unwrap().into_repr());
+    verify(pk, m, acc.into());
 
     // let sigs = sigs_strs
     //     .iter()
